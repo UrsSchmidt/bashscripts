432fa4176f45744aecba1da803ffb58b8f44ce02
UrsSchmidt <ursbjoernschmidt@googlemail.com>
2018-12-03 10:11:40 +0100

    Moved source files into src/

--- install.sh
+++ install.sh
@@ -3,12 +3,9 @@
 path='/usr/local/bin'
 
 if [ -d "$path" ]; then
-    cp 'ascii.sh' "$path/ascii"
-    cp 'distribution.sh' "$path/distribution"
-    cp 'logfilter.sh' "$path/logfilter"
-    cp 'pull.sh' "$path/pull"
-    cp 'unwindows.sh' "$path/unwindows"
-    cp 'wosut.sh' "$path/wosut"
+    for f in 'src/'*.sh; do
+        cp "$f" "$path/$(basename $f .sh)"
+    done
 else
     echo "Error: $path is not a directory"
 fi
--- uninstall.sh
+++ uninstall.sh
@@ -2,30 +2,10 @@
 
 path='/usr/local/bin'
 
-if [ ! -d "$path" ]; then
+if [ -d "$path" ]; then
+    for f in 'src/'*.sh; do
+        rm "$path/$(basename $f .sh)"
+    done
+else
     echo "Error: $path is not a directory"
-fi
-
-if [ -f "$path/ascii" ]; then
-    rm "$path/ascii"
-fi
-
-if [ -f "$path/distribution" ]; then
-    rm "$path/distribution"
-fi
-
-if [ -f "$path/logfilter" ]; then
-    rm "$path/logfilter"
-fi
-
-if [ -f "$path/pull" ]; then
-    rm "$path/pull"
-fi
-
-if [ -f "$path/unwindows" ]; then
-    rm "$path/unwindows"
-fi
-
-if [ -f "$path/wosut" ]; then
-    rm "$path/wosut"
 fi
