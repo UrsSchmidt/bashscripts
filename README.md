8ca7a735be008c94e7097c2c643abd6fff85c888
Urs-Björn Schmidt <ursbjoernschmidt@gmail.com>
2019-08-16 23:22:18 +0200

    Added patchwork.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Contains the following scripts:
  - distribution (Shows lines of code in a directory separated by file extension.)
  - logfilter (Shows an automatically filtered git log.)
  - mac (music archive checker; Checks my music archive for inconsistencies.)
+ - patchwork (Automatically applies git patches to a directory.)
  - pull (Recursively updates git and svn repositories.)
  - unwindows (Updates file permissions.)
  - wosut (Which operating system used this?)
---
+++ src/patchwork.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# patchwork -- Automatically applies git patches to a directory.
+#
+# @author Urs Schmidt
+
+if [[ "$#" -ne 2 && "$#" -ne 3 ]]
+then
+    echo 'Usage: patchwork <project> <patch> [<performCommit>]'
+    exit 1
+fi
+
+rootProject="$1"
+rootPatch="$2"
+performCommit='true'
+if [[ "$#" -eq 3 ]]
+then
+    performCommit="$3"
+fi
+verbose='true'
+
+# https://stackoverflow.com/a/8574392
+function containsElement {
+    local e match="$1"
+    shift
+    for e; do [[ "$e" == "$match" ]] && return 0; done
+    return 1
+}
+
+appliedPatches=()
+
+function applyPatch {
+    local project="${1%/}/"
+    local patch="${2%/}/"
+    local required="$3"
+
+    if containsElement "$patch" "${appliedPatches[@]}"
+    then
+        if [[ "$verbose" == 'true' ]]; then echo "[DUP] $patch"; fi
+    else
+        if [[ "$verbose" == 'true' ]]; then echo "[BGN] $patch"; fi
+
+        # default values
+        active='true'
+        requires=''
+        title="$patch"
+
+        local configFile="${patch}config.sh"
+        if [[ -f "$configFile" ]]
+        then
+            . ./"$configFile"
+        fi
+
+        if [[ "$active" == 'true' ]]
+        then
+            IFS=';' read -r -a requiresArray <<< "$requires"
+            for requiredPatch in "${requiresArray[@]}"
+            do
+                if [[ "$verbose" == 'true' ]]; then echo "[REQ] $patch requires $requiredPatch"; fi
+                applyPatch "$project" "$requiredPatch" 'true'
+            done
+
+            local patchFile="${patch}patch.diff"
+            if [[ -f "$patchFile" ]]
+            then
+                echo "[GIT] $patchFile"
+                git -C "$project" apply "$(pwd)/$patchFile"
+                if [[ "$performCommit" == 'true' ]]
+                then
+                    git -C "$project" add -A
+                    git -C "$project" commit -m "$title" -q
+                fi
+            fi
+            appliedPatches+=("$patch")
+
+            for subPatch in "$patch"*
+            do
+                if [[ -d "$subPatch" && "$(basename $subPatch)" =~ ^[^_].* ]]
+                then
+                    applyPatch "$project" "$subPatch" 'false'
+                fi
+            done
+        else
+            if [[ "$verbose" == 'true' ]]; then echo "[SKP] $patch"; fi
+            if [[ "$required" == 'true' ]]
+            then
+                echo "[ERR] REQUIRED PATCH $patch WAS SKIPPED!"
+            fi
+        fi
+
+        if [[ "$verbose" == 'true' ]]; then echo "[END] $patch"; fi
+    fi
+}
+
+applyPatch "$rootProject" "$rootPatch" 'false'
