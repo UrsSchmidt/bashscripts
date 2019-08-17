da9a984a3d84993a39e926d0bc3c07efa41afd66
Urs-Björn Schmidt <ursbjoernschmidt@gmail.com>
2019-08-17 08:39:41 +0200

    Fixed patchwork.

--- src/patchwork.sh
+++ src/patchwork.sh
@@ -34,61 +34,66 @@ applyPatch() {
     local patch="${2%/}/"
     local required="$3"
 
-    if containsElement "$patch" "${appliedPatches[@]}"
+    if [[ -d "$patch" ]]
     then
-        if [[ "$verbose" == 'true' ]]; then echo "[DUP] $patch"; fi
-    else
-        if [[ "$verbose" == 'true' ]]; then echo "[BGN] $patch"; fi
-
-        # default values
-        active='true'
-        requires=''
-        title="$patch"
-
-        local configFile="${patch}config.sh"
-        if [[ -f "$configFile" ]]
+        if containsElement "$patch" "${appliedPatches[@]}"
         then
-            . ./"$configFile"
-        fi
-
-        if [[ "$active" == 'true' ]]
-        then
-            IFS=';' read -r -a requiresArray <<< "$requires"
-            for requiredPatch in "${requiresArray[@]}"
-            do
-                if [[ "$verbose" == 'true' ]]; then echo "[REQ] $patch requires $requiredPatch"; fi
-                applyPatch "$project" "$requiredPatch" 'true'
-            done
-
-            local patchFile="${patch}patch.diff"
-            if [[ -f "$patchFile" ]]
-            then
-                echo "[GIT] $patchFile"
-                git -C "$project" apply "$(pwd)/$patchFile"
-                if [[ "$performCommit" == 'true' ]]
-                then
-                    git -C "$project" add -A
-                    git -C "$project" commit -m "$title" -q
-                fi
-            fi
-            appliedPatches+=("$patch")
-
-            for subPatch in "$patch"*
-            do
-                if [[ -d "$subPatch" && "$(basename $subPatch)" =~ ^[^_].* ]]
-                then
-                    applyPatch "$project" "$subPatch" 'false'
-                fi
-            done
+            if [[ "$verbose" == 'true' ]]; then echo "[DUP] $patch"; fi
         else
-            if [[ "$verbose" == 'true' ]]; then echo "[SKP] $patch"; fi
-            if [[ "$required" == 'true' ]]
-            then
-                echo "[ERR] REQUIRED PATCH $patch WAS SKIPPED!"
-            fi
-        fi
+            if [[ "$verbose" == 'true' ]]; then echo "[BGN] $patch"; fi
 
-        if [[ "$verbose" == 'true' ]]; then echo "[END] $patch"; fi
+            # default values
+            active='true'
+            requires=''
+            title="$patch"
+
+            local configFile="${patch}config.sh"
+            if [[ -f "$configFile" ]]
+            then
+                . ./"$configFile"
+            fi
+
+            if [[ "$active" == 'true' ]]
+            then
+                IFS=';' read -r -a requiresArray <<< "$requires"
+                for requiredPatch in "${requiresArray[@]}"
+                do
+                    if [[ "$verbose" == 'true' ]]; then echo "[REQ] $patch requires $requiredPatch"; fi
+                    applyPatch "$project" "$requiredPatch" 'true'
+                done
+
+                local patchFile="${patch}patch.diff"
+                if [[ -f "$patchFile" ]]
+                then
+                    echo "[GIT] $patchFile"
+                    git -C "$project" apply "$(pwd)/$patchFile"
+                    if [[ "$performCommit" == 'true' ]]
+                    then
+                        git -C "$project" add -A
+                        git -C "$project" commit -m "$title" -q
+                    fi
+                fi
+                appliedPatches+=("$patch")
+
+                for subPatch in "$patch"*
+                do
+                    if [[ -d "$subPatch" && "$(basename $subPatch)" =~ ^[^_].* ]]
+                    then
+                        applyPatch "$project" "$subPatch" 'false'
+                    fi
+                done
+            else
+                if [[ "$verbose" == 'true' ]]; then echo "[SKP] $patch"; fi
+                if [[ "$required" == 'true' ]]
+                then
+                    echo "[ERR] REQUIRED PATCH $patch WAS SKIPPED!"
+                fi
+            fi
+
+            if [[ "$verbose" == 'true' ]]; then echo "[END] $patch"; fi
+        fi
+    else
+        echo "[ERR] PATCH $patch DOES NOT EXIST!"
     fi
 }
 
