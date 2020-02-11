df3939f1f3ec1701cea2376500a4d21b8d857f1b
Urs-Björn Schmidt <ursbjoernschmidt@gmail.com>
2020-02-11 09:47:32 +0100

    Added more options to shrnd.

--- src/shrnd.sh
+++ src/shrnd.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
-# shrnd -- Show random; open a random image or video in either eye-of-gnome or vlc.
+# shrnd -- Show random; opens a random image or video file in either eye-of-gnome or vlc.
 #
 # @author Urs Schmidt
 
 count=1
-if [[ "$1" != '' ]]; then
-    count="$1"
+mode=-a # -a, -i, -v
+src='.'
+for arg in "$@"; do
+    if [[ "$arg" =~ ^-[aiv]$ ]]; then
+        mode="$arg"
+    elif [[ "$arg" =~ ^[0-9]+$ ]]; then
+        count="$arg"
+    else
+        src="$arg"
+    fi
+done
+
+findargs=()
+if [[ "$mode" == '-a' || "$mode" == '-i' ]]; then
+    if [[ "${#findargs[@]}" -ne 0 ]]; then
+        findargs+=(-o)
+    fi
+    findargs+=(-iname '*.bmp' -o -iname '*.gif' -o -iname '*.jpeg' -o -iname '*.jpg' -o -iname '*.png')
+fi
+if [[ "$mode" == '-a' || "$mode" == '-v' ]]; then
+    if [[ "${#findargs[@]}" -ne 0 ]]; then
+        findargs+=(-o)
+    fi
+    findargs+=(-iname '*.avi' -o -iname '*.flv' -o -iname '*.mp4' -o -iname '*.wmv')
 fi
 
-find . -type f \( -iname '*.flv' -o -iname '*.gif' -o -iname '*.jpg' -o -iname '*.mp4' -o -iname '*.png' -o -iname '*.wmv' \) | shuf -n "$count" | while IFS= read -r file; do
-    if [[ "$file" == *.gif || "$file" == *.jpg || "$file" == *.png ]]; then
+echo find "$src" -type f \( "${findargs[@]}" \)
+find "$src" -type f \( "${findargs[@]}" \) | shuf -n "$count" | while IFS= read -r file; do
+    if [[ "$file" =~ \.(bmp|gif|jpeg|jpg|png)$ ]]; then
         echo "Image: $file"
         eog --fullscreen "$file"
-    elif [[ "$file" == *.flv || "$file" == *.mp4 || "$file" == *.wmv ]]; then
+    elif [[ "$file" =~ \.(avi|flv|mp4|wmv)$ ]]; then
         echo "Video: $file"
         length=$(ffprobe -i "$file" -show_entries format=duration -v quiet -of csv="p=0")
         if [[ "$length" != '' ]]; then
